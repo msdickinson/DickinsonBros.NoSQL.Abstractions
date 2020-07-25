8899d96842abe28544522f0ff25a8e8744d6e916
Mark Dickinson <marksamdickinson@gmail.com>
2020-07-25 14:15:58 -0700

    Add project files.

---
+++ DickinsonBros.NoSQLService.Abstractions/INoSQLService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace DickinsonBros.NoSQLService.Abstractions
+{
+    public interface INoSQLService
+    {
+        Task<T> FetchAsync<T>(string id, string key);
+        Task InsertAsync<T>(string key, T value);
+        Task UpsertAsync<T>(string key, T value);
+        Task DeleteAsync(string id, string key);
+    }
+}
